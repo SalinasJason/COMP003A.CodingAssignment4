b71db872786f23218699ac760054d41503930f36
SalinasJason <jasonsalinas770@gmail.com>
2025-02-06 12:49:59 -0800

    Added top comments

--- COMP003A.CodingAssignment4/Program.cs
+++ COMP003A.CodingAssignment4/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace COMP003A.CodingAssignment4
+﻿// Author: Jason Salinas
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+// Purpose: Inventory management application with a minimum of 10 elements in the collection.
+namespace COMP003A.CodingAssignment4
 {
     internal class Program
     {
